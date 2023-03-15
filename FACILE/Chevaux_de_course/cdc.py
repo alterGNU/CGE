1d3cc353ba2e596b3e742a9c4dccd2491d76d2d7
alterGNU <alterGNU@gmail.com>
2023-03-15 12:30:20 +0100

    FACILE-Cheveaux de course-Python(OPTI)

--- FACILE/Chevaux_de_course/cdc.py
+++ FACILE/Chevaux_de_course/cdc.py
@@ -1,6 +1,6 @@
 # hypo1: sorted de python est ultra opti vs moi!(same 4 len)
 # hypo2: dans tab trié, ne comparer que les éléments contigues.
-C=sorted([*map(int,open(0))])
+C=sorted([*map(int,open(0))][1:])
 res=C[1]-C[0]
 for i in range(1,len(C)-1):
     diff = C[i+1]-C[i]
